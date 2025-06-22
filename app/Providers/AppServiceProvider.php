b2c32098b7d01a52f3d8834f6e70ad0502a31bec
MuchiriAndrew <kariukia225@gmail.com>
2025-06-22 13:56:20 +0300

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.env') === 'local') {
+        if (config('app.env') == 'local') {
             URL::forceScheme('https'); // Force HTTPS
         }
     }
