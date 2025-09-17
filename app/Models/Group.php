001d6183d7a92dca638a02825b93c7723c755a72
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2025-09-17 09:06:22 +0300

    refactor: add more fields to groups table and model

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -7,11 +7,18 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Group extends Model
 {
-    protected $fillable = ['name', 'email', 'phone_number', 'county', 'sub_county', 'address', 'township'];
+    protected $fillable = ['name', 'email', 'phone_number', 'county', 'sub_county', 'address', 'township','group_certificate','ward','local_implementing_partner_id','county_ENA_staff_id'];
 
     public function members()
     {
         return $this->hasMany(\App\Models\Member::class);
+    }
+    public function localImplementingPartner()
+    {
+        return $this->belongsTo(\App\Models\LocalImplementingPartner::class,'local_implementing_partner_id');
+    }
+    public function CountyENAStaff(){
+        return $this->belongsTo(\App\Models\CountyENAStaff::class,'county_ENA_staff_id');   
     }
     
     public function surveys()
