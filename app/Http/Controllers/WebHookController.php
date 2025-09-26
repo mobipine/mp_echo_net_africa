3ab531a1323c199a8bddb0175751d8dfc08d1a11
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2025-09-26 19:10:17 +0300

    Fix: Webhook to handle numbers starting with 0 and with 254

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -20,25 +20,40 @@ class WebHookController extends Controller
     {
         Log::info('Webhook received:', $request->all());
 
-        if (!empty($msg['from_me'])) {
-            Log::info("Ignored own message: " . ($msg['text']['body'] ?? 'N/A'));
-            return response()->json(['status' => 'ignored']);
-        }
-
         $data = $request->all();
 
+        $msisdn = null;
+        $message = null;
+
+        // Case 1: Custom payload
         if (isset($data['phone_number']) && isset($data['message'])) {
             $msisdn = $data['phone_number'];
             $message = trim(strtolower($data['message']));
         }
+
+        // Case 2: WhatsApp webhook format
         elseif (isset($data['messages'][0])) {
             $msg = $data['messages'][0];
 
+            // Ignore messages you sent yourself
+            if (!empty($msg['from_me'])) {
+                Log::info("Ignored own message: " . ($msg['text']['body'] ?? 'N/A'));
+                return response()->json(['status' => 'ignored']);
+            }
+
             $msisdn = $msg['from'] ?? null;
-            $message = isset($msg['text']['body']) 
-                ? trim(strtolower($msg['text']['body'])) 
+            $message = isset($msg['text']['body'])
+                ? trim(strtolower($msg['text']['body']))
                 : null;
         }
+
+        // Normalize MSISDN (ensure all start with 254...)
+       if ($msisdn) {
+        // If number starts with 254, change to 0...
+        if (substr($msisdn, 0, 3) === "254") {
+            $msisdn = "0" . substr($msisdn, 3);
+        }
+    }
 
         // Check if the message is a trigger word for any survey
         $survey = Survey::where('trigger_word', $message)->first();
