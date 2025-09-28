cb10dfbdf5c681759afab70feb14da89333e0ccf
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2025-09-28 14:59:16 +0300

    Feat: Refactor the whatsapp config file to include env variables needed

--- config/whatsapp.php
+++ config/whatsapp.php
@@ -1,8 +1,11 @@
 <?php
 
 return [
-    'connection' => [
-        'url' => 'https://gate.whapi.cloud/',
-        'token' => env('WA_ACCESS_TOKEN'),
-    ],
+    'access_token' => env('WA_ACCESS_TOKEN'),
+    'phone_number_id' => env('WA_PHONE_NUMBER_ID'),
+    'business_account_id' => env('WA_BUSINESS_ACCOUNT_ID'),
+    'test_phone_number_id' => env('WA_TEST_PHONE_NUMBER_ID'),
+    'test_business_account_id' => env('WA_TEST_BUSINESS_ACCOUNT_ID'),
+    'verify_token' => env('WA_VERIFY_TOKEN'),
+    'api_version' => 'v19.0',
 ];
