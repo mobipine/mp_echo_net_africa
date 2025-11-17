39722ebc2994c1675f31d7270545a73e0b53a523
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2025-11-17 08:51:41 +0300

    Feat: Validate date of birth when creating and editing a member to be between 18-35 years and 18-40 years for persons with disability

--- app/Filament/Resources/MemberResource/Pages/EditMember.php
+++ app/Filament/Resources/MemberResource/Pages/EditMember.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\MemberResource\Pages;
 
 use App\Filament\Resources\MemberResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Validation\ValidationException;
 
 class EditMember extends EditRecord
 {
@@ -22,5 +24,22 @@ class EditMember extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
-    
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        try{
+            MemberResource::validateDob($data);
+        } catch (ValidationException $e) {
+        // Show a notification in the UI
+        Notification::make()
+            ->title('Member not saved')
+            ->danger()
+            ->body(implode(' ', $e->errors()['dob'])) // Show the dob validation message
+            ->send();
+
+        // Re-throw to prevent save
+        throw $e;
+        }
+        //  // <-- validate here
+         return $data;
+    }
 }
